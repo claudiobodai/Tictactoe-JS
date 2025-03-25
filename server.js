66f3cda0ce150e721424787bdd254901be6b219a
claudiobodai <bodaiclaudio@icloud.com>
2025-03-25 15:57:36 +0100

    port

--- server.js
+++ server.js
@@ -368,6 +368,7 @@ function checkWinner(board) {
     return null;
 }
 
-server.listen(4000, () => {
-    console.log('Server in esecuzione su http://localhost:4000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
